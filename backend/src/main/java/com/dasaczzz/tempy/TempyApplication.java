4f6cde18da366ff60d62ef25ea010b61df8d04de
dasaczzz <118865516+dasaczzz@users.noreply.github.com>
2026-04-07 11:10:04 -0500

    first commit: adding only the backend config files

---
+++ backend/src/main/java/com/dasaczzz/tempy/TempyApplication.java
@@ -0,0 +1,13 @@
+package com.dasaczzz.tempy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dasaczzz/tempy/TempyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasaczzz.tempy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
